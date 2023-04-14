362da741363d76ec7d4c4e5a6ee5c50d83c44ec3
ptitto <paulustito555@gmail.com>
2023-04-14 09:40:03 +0700

    Fix

--- Analytical Query/AnalyticalQuery.sql
+++ Analytical Query/AnalyticalQuery.sql
@@ -6,7 +6,7 @@ right join car_product cp on (cp.product_id = b.car_product_id)
 group by 1
 
 
---
+-- Membandingkan harga mobil berdasarkan harga rata-rata per kota.
 
 with avg_car_city as (
 	SELECT c.nama_kota, avg(cp.price) as avg_car_city FROM car_product cp
